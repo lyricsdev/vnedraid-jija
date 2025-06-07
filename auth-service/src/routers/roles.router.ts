b0b0307bd0f4c91ea333090d13e0cae9c809e46e
Qulllaia <znurock@mail.ru>
2025-06-07 22:00:57 +0300

    jija test

--- auth-service/src/routers/roles.router.ts
+++ auth-service/src/routers/roles.router.ts
@@ -3,10 +3,10 @@ const router2 = require('express').Router()
 const controller = require('../controllers/roles.controller')
 const middlewareAuthForRoles = require('../middleware/auth.middleware')
 
-router2.post('/reassign',middlewareAuthForRoles, controller.reassignRoles)
-router2.post('/remove',middlewareAuthForRoles, controller.removeRoles)
-router2.post('/permission',middlewareAuthForRoles, controller.postPermission)
-router2.get('/permission',middlewareAuthForRoles, controller.getPermission)
+router2.post('/reassign', controller.reassignRoles)
+router2.post('/remove', controller.removeRoles)
+router2.post('/permission', controller.postPermission)
+router2.get('/permission', controller.getPermission)
 
 
 
