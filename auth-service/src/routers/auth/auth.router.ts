810ff8208042722026b2aa4ccbfd0648538f8658
lyricsdev <lordmzc@icloud.com>
2025-06-07 23:17:49 +0300

    :)))

--- auth-service/src/routers/auth/auth.router.ts
+++ auth-service/src/routers/auth/auth.router.ts
@@ -8,4 +8,5 @@ router.post('/verify', cont.verifyAccess)
 
 
 
+
 module.exports = router
--- auth-service/src/routers/user/user.router.ts
+++ auth-service/src/routers/user/user.router.ts
@@ -4,4 +4,11 @@ const userController = require('../../controllers/user.controller')
 
 userRouter2.get('/:id', userController.getUsers)
 
+const controller2 = require('../../controllers/roles.controller')
+
+userRouter2.post('/reassign', controller2.reassignRoles)
+userRouter2.post('/remove', controller2.removeRoles)
+userRouter2.post('/permission', controller2.postPermission)
+userRouter2.get('/permission', controller2.getPermission)
+
 module.exports = userRouter2
