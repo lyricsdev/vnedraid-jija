ae237524f49d9fd0ed62fd09c5e221918a7d320c
kash1ma <ayollo.matvey@gmail.com>
2025-06-08 00:26:59 +0300

    feat: added logout endpoint

--- auth-service/src/controllers/auth.controller.ts
+++ auth-service/src/controllers/auth.controller.ts
@@ -1,20 +1,20 @@
 import { Request, Response } from "express-serve-static-core";
 import { ParsedQs } from "qs";
 import { AuthDataset, AuthResponse } from "./types/auth.types";
-import { Prisma } from '@prisma/client';
+import { Prisma } from "@prisma/client";
 
-const prismaService = require('../database/prisma.service');
-const passwordUtils = require('../utils/password.utils')
-const utilsJwt = require('../utils/jwt.utils')
+const prismaService = require("../database/prisma.service");
+const passwordUtils = require("../utils/password.utils");
+const utilsJwt = require("../utils/jwt.utils");
 
-class AuthController{
-
-    async postRegisterUser(req: Request<{}, any, any, ParsedQs, Record<string, any>>, res: Response<any, Record<string, any>, number>){
-    
-        const {name, password} = req.body;
-        try{
-
-            await prismaService.$executeRaw`
+class AuthController {
+  async postRegisterUser(
+    req: Request<{}, any, any, ParsedQs, Record<string, any>>,
+    res: Response<any, Record<string, any>, number>,
+  ) {
+    const { name, password } = req.body;
+    try {
+      await prismaService.$executeRaw`
             INSERT INTO User (name, password, createdAt)
             VALUES (
                 ${name},
@@ -22,109 +22,131 @@ class AuthController{
                 NOW()
                 )
                 `;
-        }catch(e){
-            return res.status(404).send({
-            code: 404,
-            message: "Данный пользователь существует"
-            })
-        }
+    } catch (e) {
+      return res.status(404).send({
+        code: 404,
+        message: "Данный пользователь существует",
+      });
+    }
 
-           
-        const [user] = await prismaService.$queryRaw<{id: number}[]>`
+    const [user] = await prismaService.$queryRaw<{ id: number }[]>`
             SELECT id FROM User WHERE name = ${name} LIMIT 1
         `;
-        
-        const users = await prismaService.$queryRaw`
-        SELECT 
+
+    const users = await prismaService.$queryRaw`
+        SELECT
             *
         FROM User where name = ${name}
         `;
 
-        if(!users[0]) return res.status(404).send({
-            code: 404,
-            message: "Ошибка авторизации"
-        })
-            
-        const returnObject: AuthDataset = {
-            id: users[0].id,
-            name: users[0].name,
-            createdAt: users[0].createdAt
-        } 
-        const res2 = utilsJwt.generateToken(returnObject)
+    if (!users[0])
+      return res.status(404).send({
+        code: 404,
+        message: "Ошибка авторизации",
+      });
 
-        res.cookie('token', res2, {
-            httpOnly: true, 
-            secure: true, 
-            sameSite: 'strict', 
-            maxAge: 24 * 60 * 60 * 1000,
-        });
+    const returnObject: AuthDataset = {
+      id: users[0].id,
+      name: users[0].name,
+      createdAt: users[0].createdAt,
+    };
+    const res2 = utilsJwt.generateToken(returnObject);
 
-        return res.status(200).send(returnObject)
-    }
+    res.cookie("token", res2, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
 
-    async postLoginUser(req: Request<{}, any, any, ParsedQs, Record<string, any>>, res: Response<any, Record<string, any>, number>){
-        const {name, password} = req.body;
-         const user = await prismaService.$queryRaw`
+    return res.status(200).send(returnObject);
+  }
+
+  async postLoginUser(
+    req: Request<{}, any, any, ParsedQs, Record<string, any>>,
+    res: Response<any, Record<string, any>, number>,
+  ) {
+    const { name, password } = req.body;
+    const user = await prismaService.$queryRaw`
             SELECT * FROM User WHERE name = ${name} LIMIT 1
         `;
-        if(!user[0]) return res.status(404).send({
-            code: 404,
-            "message":"Данного пользователя не существует"
-        })
-        const varify = await passwordUtils.verifyPassword(password, user[0].password)
+    if (!user[0])
+      return res.status(404).send({
+        code: 404,
+        message: "Данного пользователя не существует",
+      });
+    const varify = await passwordUtils.verifyPassword(
+      password,
+      user[0].password,
+    );
 
-        if(!varify) return res.status(402).send({
-            code: 402,
-            message: "Указанные данные пользователя неверны"
-        })
+    if (!varify)
+      return res.status(402).send({
+        code: 402,
+        message: "Указанные данные пользователя неверны",
+      });
 
-        if(!user[0]) return res.status(404).send({
-            code: 404,
-            message: "Ошибка авторизации"
-        })
-            
-        const userPerm = await prismaService.$queryRaw`
-        SELECT 
+    if (!user[0])
+      return res.status(404).send({
+        code: 404,
+        message: "Ошибка авторизации",
+      });
+
+    const userPerm = await prismaService.$queryRaw`
+        SELECT
             *
         FROM UserPermission where userId = ${user[0].id}
         `;
-        let roles = [];
-        for(let i = 0; i < userPerm.length; i++){
-            roles.push(...await prismaService.$queryRaw`
-                SELECT 
+    let roles = [];
+    for (let i = 0; i < userPerm.length; i++) {
+      roles.push(
+        ...(await prismaService.$queryRaw`
+                SELECT
                     *
                 FROM Permission where id = ${userPerm[i].permissionId}
-            `)
-        }
-
-        
-        const returnObject: AuthDataset = {
-            id: user[0].id,
-            name: user[0].name,
-            createdAt: user[0].createdAt,
-            roles
-        } 
-        const res2 = utilsJwt.generateToken(returnObject)
-
-        res.cookie('token', res2, {
-            httpOnly: true, 
-            secure: true, 
-            sameSite: 'strict', 
-            maxAge: 24 * 60 * 60 * 1000,
-        });
-
-        
-        return res.status(200).send(returnObject)
+            `),
+      );
     }
 
-    async verifyAccess(req: Request<{}, any, any, ParsedQs, Record<string, any>>, res: Response<any, Record<string, any>, number>){
-        const token = req.cookies?.token;
-        if(!token) res.status(401).json({"verify": false});
-        const isVerified = utilsJwt.verifyToken(token)
-        if(isVerified) return res.json({"verify": true});
-        return res.json({"verify": false});
-    }
+    const returnObject: AuthDataset = {
+      id: user[0].id,
+      name: user[0].name,
+      createdAt: user[0].createdAt,
+      roles,
+    };
+    const res2 = utilsJwt.generateToken(returnObject);
+
+    res.cookie("token", res2, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    return res.status(200).send(returnObject);
+  }
+
+  async postLogoutUser(
+    req: Request<{}, any, any, ParsedQs, Record<string, any>>,
+    res: Response<any, Record<string, any>, number>,
+  ) {
+    res.clearCookie("token");
+    return res.status(200).send({
+      code: 200,
+      message: "Succesfuly logout",
+    });
+  }
+
+  async verifyAccess(
+    req: Request<{}, any, any, ParsedQs, Record<string, any>>,
+    res: Response<any, Record<string, any>, number>,
+  ) {
+    const token = req.cookies?.token;
+    if (!token) res.status(401).json({ verify: false });
+    const isVerified = utilsJwt.verifyToken(token);
+    if (isVerified) return res.json({ verify: true });
+    return res.json({ verify: false });
+  }
 }
 
-module.exports = new AuthController()
-
+module.exports = new AuthController();
--- auth-service/src/routers/auth/auth.router.ts
+++ auth-service/src/routers/auth/auth.router.ts
@@ -1,12 +1,10 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
-const cont = require('../../controllers/auth.controller')
+const cont = require("../../controllers/auth.controller");
 
-router.post('/register', cont.postRegisterUser)
-router.post('/login', cont.postLoginUser)
-router.post('/verify', cont.verifyAccess)
+router.post("/register", cont.postRegisterUser);
+router.post("/login", cont.postLoginUser);
+router.post("/verify", cont.verifyAccess);
+router.post("/logout", cont.postLogoutUser);
 
-
-
-
-module.exports = router
+module.exports = router;
