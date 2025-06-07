aca6bfe25b5caddfad41b0fc8f0e69b0adc770ee
lyricsdev <lordmzc@icloud.com>
2025-06-07 17:50:33 +0300

    fixes

--- k8s/src/index.ts
+++ k8s/src/index.ts
@@ -3,10 +3,13 @@ import dotenv from 'dotenv';
 import k8sRouter from './controllers/k8s'
 import projectRouter from './controllers/project'
 import { getClusterInfo } from './service/cluster/k8s';
-
+import cors from 'cors'
 dotenv.config();
 
 const app = express();
+app.use(cors({
+  origin: "*"
+}))
 app.use(express.json());
 app.use('/api/k8s/cluster', k8sRouter)
 app.use('/api/k8s/project', projectRouter)
